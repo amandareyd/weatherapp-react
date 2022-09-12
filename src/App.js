14ca0da6a7a1284995f872f2783bdc49aca03190
amandareyd <104109018+amandareyd@users.noreply.github.com>
2022-09-12 19:42:02 -0400

    changed title and added bootsrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-   
-      
-      </header>
-      <footer>
+      <div className="container">
+        <footer>
         This project was coded by Amanda Drause and is {""}
         <a
         href="https://github.com/amandareyd/weatherapp-react.git" target="blank" rel="">
         open sourced on Github
         </a>
         </footer>
-            </div>
+      </div>
+      
+    </div>
 
   );
 }
