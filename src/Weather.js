1bda472d86e4b1b609c5c0e828b1922e0044ae3d
amandareyd <104109018+amandareyd@users.noreply.github.com>
2022-11-10 11:19:51 -0500

    added date

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, {useState}from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -9,13 +10,13 @@ export default function Weather(props){
     console.log(response.data);
     setWeatherData({
       ready:true,
-      temperature:response.data.temperature,
+      temperature:response.data.temperature.current,
       humidity: response.data.temperature.humidity,
       description:response.data.condition.description,
       wind: response.data.wind.speed,
-      date:"Wednesday 7:00",
+      date:new Date(response.data.time*1000),
       city: response.data.city,
-      iconUrl:`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png`,
+      iconUrl:"http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png",
     });
     
   
@@ -43,7 +44,9 @@ export default function Weather(props){
     </form>
     <h1> {weatherData.city}</h1>
     <ul>
-      <li>{weatherData.date}</li>
+      <li>
+        <FormattedDate date={weatherData.date}/>
+        </li>
       <li className="text-capitalize">{weatherData.description}</li>
     </ul>
     <div className="row mt-3">
